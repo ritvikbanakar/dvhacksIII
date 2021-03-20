8ae0a8e4a910ceced064f69dce5d0c1042119059
Siddhant Kumar <sid625@gmail.com>
2021-03-20 16:54:29 -0700

    Finished Connecting the Camera to the App

--- DVHacks3/DVHacks3/DilationTestVC.swift
+++ DVHacks3/DVHacks3/DilationTestVC.swift
@@ -10,38 +10,43 @@ import AVKit
 import Vision
 import CoreML
 
-class DilationTestVC: UIViewController {
+class DilationTestVC: UIViewController, AVCapturePhotoCaptureDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+
+    @IBOutlet weak var newImage: UIImageView!
+    
+    @IBOutlet weak var secondView: UIView!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let captureSession = AVCaptureSession()
+       
         
-        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .front) else { fatalError("no front camera. but don't all iOS 10 devices have them?")}
-            
-    
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
-        captureSession.addInput(input)
-        captureSession.startRunning()
-        
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        
-        view.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
-        
-//        VNImageRequestHandler(
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  
+    @IBAction func takePicture(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
-    */
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else{
+            return
+        }
+        newImage.image = image
+        
+    }
 
 }
+
