1a76127b338d36cdcefc9d8874eb7102b2080d10
Siddhant Kumar <sid625@gmail.com>
2021-03-20 17:35:03 -0700

    Finish Pupil Dilation Test

--- DVHacks3/DVHacks3/DilationTestVC.swift
+++ DVHacks3/DVHacks3/DilationTestVC.swift
@@ -10,21 +10,20 @@ import AVKit
 import Vision
 import CoreML
 
-class DilationTestVC: UIViewController, AVCapturePhotoCaptureDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+class DilationTestVC: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var newImage: UIImageView!
     
     @IBOutlet weak var secondView: UIView!
-    
+    var model = EyeClassifer()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
-        
-    }
     
+
+
+    }
   
     @IBAction func takePicture(_ sender: Any) {
         let picker = UIImagePickerController()
@@ -44,9 +43,79 @@ class DilationTestVC: UIViewController, AVCapturePhotoCaptureDelegate, UIImagePi
         guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else{
             return
         }
-        newImage.image = image
         
+        var editedImage = image.scaleImage(toSize: CGSize(width: 299, height: 299))
+        
+        let dilation_data = try? model.prediction(image: (editedImage?.pixelBuffer())!)
+        
+        let result = dilation_data!.classLabel
+                
+//                model.prediction(solarPanels: solarPanels, greenhouses: greenhouses, size: size) else {
+//            fatalError("Unexpected runtime error.")
+//        }
     }
 
+}
+
+
+extension UIImage {
+    func scaleImage(toSize newSize: CGSize) -> UIImage? {
+        var newImage: UIImage?
+        let newRect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height).integral
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        if let context = UIGraphicsGetCurrentContext(), let cgImage = self.cgImage {
+            context.interpolationQuality = .high
+            let flipVertical = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: newSize.height)
+            context.concatenate(flipVertical)
+            context.draw(cgImage, in: newRect)
+            if let img = context.makeImage() {
+                newImage = UIImage(cgImage: img)
+            }
+            UIGraphicsEndImageContext()
+        }
+        return newImage
+    }
+    
+    func pixelBuffer() -> CVPixelBuffer? {
+        let width = self.size.width
+        let height = self.size.height
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+                     kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                         Int(width),
+                                         Int(height),
+                                         kCVPixelFormatType_32ARGB,
+                                         attrs,
+                                         &pixelBuffer)
+
+        guard let resultPixelBuffer = pixelBuffer, status == kCVReturnSuccess else {
+            return nil
+        }
+
+        CVPixelBufferLockBaseAddress(resultPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(resultPixelBuffer)
+
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let context = CGContext(data: pixelData,
+                                      width: Int(width),
+                                      height: Int(height),
+                                      bitsPerComponent: 8,
+                                      bytesPerRow: CVPixelBufferGetBytesPerRow(resultPixelBuffer),
+                                      space: rgbColorSpace,
+                                      bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+                                        return nil
+        }
+
+        context.translateBy(x: 0, y: height)
+        context.scaleBy(x: 1.0, y: -1.0)
+
+        UIGraphicsPushContext(context)
+        self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(resultPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+
+        return resultPixelBuffer
+    }
 }
 
